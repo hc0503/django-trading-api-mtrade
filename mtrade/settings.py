2f6c1eb70b39d14b6a6ce7ce7e39471ff579c22c
Jonathan Robin Langford <jrobinlangford@gmail.com>
2021-02-15 23:15:46 -0600

    Remove uninstalled django nose app from settings

--- mtrade/settings.py
+++ mtrade/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mtrade.domain.users',
-    'django_nose',
     'rest_framework',
     'drf_spectacular',
 ]
