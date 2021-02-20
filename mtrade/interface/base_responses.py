854d4a3347ee38083d15b0434ba82da07e1e6d21
Jonathan Robin Langford <jrobinlangford@gmail.com>
2021-02-19 18:00:26 -0600

    Add missing base_responses.py

---
+++ mtrade/interface/base_responses.py
@@ -0,0 +1,8 @@
+from rest_framework.exceptions import APIException
+
+class BadRequest(APIException):
+    status_code = 400
+    default_detail = 'The request cannot be fulfilled, please try again with different parameters.'
+    default_code = 'bad_request'
+
+
