f4af486a0fb05982e60ab176f7a09f28ae24a567
Devdreamsolution <q3construction1@gmail.com>
2021-02-26 23:57:21 +1000

    Remove wrong, test logger

--- mtrade/infrastructure/logger/views.py
+++ mtrade/infrastructure/logger/views.py
@@ -1,17 +1,9 @@
 # django import
 from django.http import HttpResponse
-from rest_framework import status, viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
+import logging
 
-# local import
-from .models import Logger
-from .serializers import LoggerSerializer
+logger = logging.getLogger('mtrade_log')
 
-class LoggerViewSet(viewsets.ModelViewSet):
-	"""
-	API endpoint that allows loggers to be viewed or edited
-	"""
-	queryset = Logger.objects.all().order_by('created_at')
-	serializer_class = LoggerSerializer
+def testLogger(request):
+	logger.info('The index is called successfully.', extra={'referral_code': '52d6ce'})
+	return HttpResponse("Hello logging test.")
