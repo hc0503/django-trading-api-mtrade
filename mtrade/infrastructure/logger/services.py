4b8a96d11e5ad9f5c56dedd39ed5500667712723
Devdreamsolution <q3construction1@gmail.com>
2021-03-01 09:03:57 +1000

    Logger: change timestamp, add module

--- mtrade/infrastructure/logger/services.py
+++ mtrade/infrastructure/logger/services.py
@@ -2,15 +2,19 @@
 import logging
 import ujson
 import json_log_formatter
+import datetime
 
 class CustomisedJSONFormatter(json_log_formatter.JSONFormatter):
     json_lib = ujson
 
-    def json_record(self, message: str, extra: dict, record: logging.LogRecord) -> dict:
+    def json_record(self, message, extra, record):
         extra['msg'] = message
-
-        # Include builtins
-        extra['level'] = record.levelname
-        extra['time'] = record.created
-
+        extra['level'] = record.__dict__['levelname']
+        extra['module'] = record.__dict__['pathname']
+        extra['time'] = datetime.datetime.now()
+        
+        request = extra.pop('request', None)
+        if request:
+            extra['x_forward_for'] = request.META.get('X-FORWARD-FOR')
+        
         return extra
