0257e961ebb946d32bb22b6596a110d529fa9b58
Devdreamsolution <q3construction1@gmail.com>
2021-03-01 12:51:45 +1000

    Logger: change test logger route to rest_framework

--- mtrade/infrastructure/logger/views.py
+++ mtrade/infrastructure/logger/views.py
@@ -1,9 +1,18 @@
 # django import
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 import logging
 
-logger = logging.getLogger('mtrade_log')
+logger = logging.getLogger(__name__)
 
+@api_view(['GET'])
 def testLogger(request):
+	"""
+	API endpoint that tests logger to be exported JSON log.
+	"""
+
 	logger.info('The index is called successfully.', extra={'referral_code': '52d6ce'})
-	return HttpResponse("Hello logging test.")
+
+	return Response({
+		'message': 'Logger testing: check the log.json file in logs folder.'
+	})
