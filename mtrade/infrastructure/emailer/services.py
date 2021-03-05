e64c370879e0b50a6fe5ef56637fa9ed109eedf4
Devdreamsolution <q3construction1@gmail.com>
2021-03-05 16:10:27 +1000

    Emailer: include cc and bcc in send method

--- mtrade/infrastructure/emailer/services.py
+++ mtrade/infrastructure/emailer/services.py
@@ -1,55 +1,32 @@
 # import django
 from django.core.mail import EmailMessage
 
-class EmailerServices():
-    def __init__(self, templateId, templateData, fromEmail, replyTo=None):
-        self.templateId = templateId
-        self.templateData = templateData
-        self.replyTo = replyTo
-        self.fromEmail = fromEmail
+class Mail():
+    def __init__(self, from_email, to, template_id, dynamic_template_data, **kwargs):
+        self.from_email = from_email
+        self.to = to
+        self.template_id = template_id
+        self.dynamic_template_data = dynamic_template_data
 
-    def sendTo(self, toEmails=[]):
-        msg = EmailMessage(
-            from_email = self.fromEmail,
-            to = toEmails,
-        )
-        msg.template_id = self.templateId
-        msg.template_data = self.templateData
-        msg.reply_to = self.replyTo
-
-        try:
-            msg.send(fail_silently=False)
-        except Exception as e:
-            raise Exception(e)
-        
-        return True
+        self.__dict__.update(kwargs)
     
-    def sendCC(self, toEmails=[], ccEmails=[]):
+    def send(self):
         msg = EmailMessage(
-            from_email = self.fromEmail,
-            to = toEmails,
-            cc = ccEmails
+            from_email = self.from_email,
+            to = self.to,
         )
-        msg.template_id = self.templateId
-        msg.template_data = self.templateData
-        msg.reply_to = self.replyTo
-
-        try:
-            msg.send(fail_silently=False)
-        except Exception as e:
-            raise Exception(e)
         
-        return True
-    
-    def sendBCC(self, toEmails=[], bccEmails=[]):
-        msg = EmailMessage(
-            from_email = self.fromEmail,
-            to = toEmails,
-            bcc = bccEmails
-        )
-        msg.template_id = self.templateId
-        msg.template_data = self.templateData
-        msg.reply_to = self.replyTo
+        msg.template_id = self.template_id
+        msg.dynamic_template_data = self.dynamic_template_data
+        
+        if 'reply_to' in globals():
+            msg.reply_to = self.reply_to
+
+        if 'cc' in globals():
+            msg.cc = self.cc
+        
+        if 'bcc' in globals():
+            msg.bcc = self.bcc
 
         try:
             msg.send(fail_silently=False)
