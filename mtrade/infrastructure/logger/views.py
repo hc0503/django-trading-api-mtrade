78546e783188d8ae95a2730e778d6df4a1465121
Devdreamsolution <q3construction1@gmail.com>
2021-03-09 22:04:07 +1000

    Logger: add authentication and update unit test

--- mtrade/infrastructure/logger/views.py
+++ mtrade/infrastructure/logger/views.py
@@ -1,17 +1,22 @@
 # django import
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 import logging
 
 logger = logging.getLogger(__name__)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def infoLogger(request):
 	"""
 	API endpoint that tests info logger to be exported JSON log.
 	"""
 
-	logger.info('The info logger is called successfully.', extra={'referral_code': '52d6ce'})
+	try:
+		logger.info('The info logger is called successfully.', extra={'referral_code': '52d6ce'})
+	except Exception as e:
+		raise APIException(e)
 
 	return Response({
 		'message': 'Logger info testing: check the log.json file in logs folder.'
