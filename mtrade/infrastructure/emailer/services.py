c340cbeda607f8759842d614b017f562c4d41fcb
Jonathan Robin Langford <jrobinlangford@gmail.com>
2021-03-09 08:29:41 -0600

    Add sendgrid vars to Mailer initialization

--- mtrade/infrastructure/emailer/services.py
+++ mtrade/infrastructure/emailer/services.py
@@ -2,7 +2,9 @@
 from django.core.mail import EmailMessage
 
 class Mail(EmailMessage):
-    def send(self):
-        result = super(Mail, self).send()
-
-        return result
+    def __init__(self, subject='', body='', from_email=None, to=None, bcc=None,
+                 connection=None, attachments=None, headers=None, cc=None,
+                 reply_to=None, template_id=None, dynamic_template_data=None):
+        super().__init__(subject, body, from_email, to, bcc, connection, attachments, headers, cc, reply_to)
+        self.template_id = template_id
+        self.dynamic_template_data = dynamic_template_data
