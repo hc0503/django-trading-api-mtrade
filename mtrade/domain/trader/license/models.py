902b027652cb34705eccffaa9251826ede536f8c
Jonathan Robin Langford <jrobinlangford@gmail.com>
2021-03-10 16:52:22 -0600

    Add trader license to domain

--- mtrade/domain/trader/license/models.py
+++ mtrade/domain/trader/license/models.py
@@ -1,6 +1,11 @@
+from dataclasses import dataclass
+import uuid
+
 from django.db import models
 
 from lib.django import custom_models
+from lib.ddd.exceptions import VOValidationExcpetion
+
 
 class TraderLicense(custom_models.DatedModel):
     """
@@ -9,3 +14,56 @@ class TraderLicense(custom_models.DatedModel):
     id = models.UUIDField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     short_description = models.CharField(max_length=250)
+
+
+@dataclass(frozen=True)
+class TraderLicenseID():
+    """
+    This is a value object that should be used to generate and pass the
+    TraderLicenseID to the TraderFactory
+    """
+    value: uuid.UUID
+
+
+@dataclass(frozen=True)
+class TraderLicenseMetadata():
+    """
+    This is a value object that should be used to generate and pass the
+    TraderLicenseMetadata to the TraderFactory
+    """
+    name: str
+    short_description: str
+
+    def __post_init__(self):
+        self.validate_name()
+        self.validate_short_description()
+
+    def validate_name(self):
+        if self.name == "":
+            raise VOValidationExcpetion(
+                "TraderLicenseMetadata name", "cannot be empty")
+        if len(self.name) > 100:
+            raise VOValidationExcpetion(
+                "TraderLicenseMetadata name", "Invalid length")
+
+    def validate_short_description(self):
+        if len(self.short_description) > 250:
+            raise VOValidationExcpetion(
+                "TraderLicenseMetadata short_description",
+                "Invalid length")
+
+
+class TraderLicenseFactory():
+    @staticmethod
+    def build_entity(
+            tl_id=TraderLicenseID,
+            tl_md=TraderLicenseMetadata) -> TraderLicense:
+        return TraderLicense(
+            id=tl_id.value, name=tl_md.name,
+            short_description=tl_md.short_description)
+
+    @classmethod
+    def build_entity_with_id(
+            cls, tl_md=TraderLicenseMetadata) -> TraderLicense:
+        tl_id = TraderLicenseID(uuid.uuid4())
+        return cls.build_entity(tl_id, tl_md)
---
+++ mtrade/domain/trader/license/test_helper.py
@@ -0,0 +1,24 @@
+import typing
+
+from django.utils.crypto import get_random_string
+
+from .models import (
+    TraderLicense,
+    TraderLicenseFactory,
+    TraderLicenseMetadata,
+)
+
+def gen_rand_license_metadata() -> TraderLicenseMetadata:
+    return TraderLicenseMetadata(
+            name = 'license'+get_random_string(36),
+            short_description = 'description'+get_random_string(100)
+            )
+
+def gen_rand_license() -> TraderLicense:
+    return TraderLicenseFactory.build_entity_with_id(gen_rand_license_metadata())
+
+def gen_rand_licenses(num_of_licenses: int) -> typing.List[TraderLicense]:
+    licenses = []
+    for _ in range(num_of_licenses):
+        licenses.append(gen_rand_license)
+    return licenses
