9500dabf6a3942a1848d30809506207b5bce54f8
bestsoftdev <bestsoftdev2020@yandex.ru>
2021-03-12 22:49:24 +0300

    Fixed ordering issue

---
+++ mtrade/interface/notification/open_api.py
@@ -0,0 +1,9 @@
+from drf_spectacular.utils import extend_schema
+
+from lib.open_api.custom_params import default_cursor_param
+
+notification_list_extension=extend_schema(
+    parameters = [
+        default_cursor_param
+    ]
+)
--- mtrade/interface/notification/setting/views.py
+++ mtrade/interface/notification/setting/views.py
@@ -2,20 +2,26 @@
 
 # django imports
 from rest_framework import permissions
+from drf_spectacular.utils import extend_schema_view
 
 # app imports
 from lib.django.custom_views import CreateListUpdateRetrieveViewSet
 from mtrade.application.notifications.setting.services import NotificationSettingAppServices as nsas
 
 # local imports
+from . import open_api
 from .serializers import NotificationSettingSerializer
 
+@extend_schema_view(
+    list=open_api.notification_setting_list_extension,
+)
 class NotificationSettingViewSet(CreateListUpdateRetrieveViewSet):
     """
     Allows clients to perform retrieve and list NotificationSettings
     """
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = NotificationSettingSerializer
+    ordering = ['-created_at']
 
     def get_queryset(self):
         return nsas.list_notification_settings(self.request.user)
